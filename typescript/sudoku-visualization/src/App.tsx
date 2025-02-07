dc6c47b9d6fb710850094064157c0d8067592f2e
arturchichorro <arturchichorro@gmail.com>
2025-02-07 18:00:22 +0000

    added simple vite app with visualization of backtracking algo

---
+++ typescript/sudoku-visualization/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Sudoku from './Sudoku/sudokuVisualization'
+// import SudokuSolver from './SudokuSolver'
+
+function App() {
+
+
+  return (
+    <>
+      <Sudoku />
+    </>
+  )
+}
+
+export default App
