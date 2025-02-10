45e2a05d4487ca8c4ff01377b92dbd8ab72d9dd6
arturchichorro <arturchichorro@gmail.com>
2025-02-10 17:47:35 +0000

    created backtracking alg function that counts recursive calls

--- sudoku_backtracking.py
+++ sudoku_backtracking.py
@@ -15,3 +15,24 @@ def solve_sudoku_backtracking(sudoku_grid):
                     return False
         return True
     backtrack(sudoku_grid)
+
+def solve_sudoku_backtracking_counting_nodes(sudoku_grid):
+    node_counter = [0]
+
+    def backtrack(sudoku_grid):
+        node_counter[0] += 1
+        for row in range(9):
+            for col in range(9):
+                if sudoku_grid[row, col] == 0:
+                    for num in range(1, 10):
+                        if is_valid_placement(sudoku_grid, row, col, num):
+                            sudoku_grid[row, col] = num
+                            if backtrack(sudoku_grid):
+                                return True
+                            sudoku_grid[row, col] = 0
+                    return False
+        return True
+
+    backtrack(sudoku_grid)
+    
+    return node_counter[0]
